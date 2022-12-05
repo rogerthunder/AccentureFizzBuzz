aa63f1af04dd2488843e1fe74c64c3a36f0fae5a
alan_ <alan_@DESKTOP-A623MBK>
2022-12-04 23:41:46 -0700

    Add project files.

---
+++ AccentureBuzzFizz/Controllers/FizzBuzzController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccentureBuzzFizz.Controllers
+{
+    [Route("api/[fizzbuzz]")]
+    [ApiController]
+    public class FizzBuzzController : ControllerBase
+    {
+
+    }
+}
